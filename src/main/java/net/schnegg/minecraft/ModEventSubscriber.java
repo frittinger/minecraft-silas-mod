c561e29e387dd15337de33508f0bb0dcd2bbf1ab
Frank Rittinger <frank.rittinger@virtual-identity.com>
2019-12-30 23:13:20 +0100

    commented out generic event registration

--- src/main/java/net/schnegg/minecraft/ModEventSubscriber.java
+++ src/main/java/net/schnegg/minecraft/ModEventSubscriber.java
@@ -1,23 +1,21 @@
 package net.schnegg.minecraft;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistryEntry;
-import net.schnegg.minecraft.init.ModItemGroups;
 
 @Mod.EventBusSubscriber(modid = SilasMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
 
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(
-                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "silas_item")
-        );
+//        event.getRegistry().registerAll(
+//                setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "silas_item")
+//        );
     }
 
     public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
@@ -31,8 +29,8 @@ public class ModEventSubscriber {
 
     @SubscribeEvent
     public static void onRegisterBlocks(RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(
-                setup(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)), "silas_block")
-        );
+//        event.getRegistry().registerAll(
+//                setup(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)), "silas_block")
+//        );
     }
 }
